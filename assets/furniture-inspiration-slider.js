a905b94304b979295233c789fd25f96c27fe610e
Francisco Duran <frandugo@ymail.com>
2024-11-26 15:31:31 -0500

    Add changes to style

---
+++ assets/furniture-inspiration-slider.js
@@ -0,0 +1,11 @@
+jQuery('.furniture-inspiration__slider-js').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    swipe: true,
+    variableWidth: true,
+    swipeToSlide: true,
+    cssEase: 'ease-in-out',
+  });
+  
